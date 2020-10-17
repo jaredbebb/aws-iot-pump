6c270b2e1052f3a0e80d98a797f60452b52834e7
Jared Bebb <j.bebb@live.com>
2020-10-17 12:56:13 -0700

    added yield() to prevent long blocking while loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,37 @@
 #include <Ticker.h>
 
 Ticker pumpticker;
-unsigned long period = 30*1000;
-unsigned long execution = 10*1000;
+unsigned long period = 30000;//14400*1000;
+unsigned long execution = 15000;//20*1000;
 int relayPin = D8;
-int state = 1;
+int state;
 
-void relay_pin_low(){
-  state = 1;
-  digitalWrite(relayPin, LOW);
-  delay(execution);
+void relay_pin_state(){
+    state = 1;
+    Serial.println("state = 1");
 }
 
 void setup() {
-  // initialize pin as output
-  pinMode(relayPin, OUTPUT);
-  Serial.begin(115200);
-  pumpticker.attach_ms_scheduled(period, relay_pin_low);
+    // initialize pin as output
+    Serial.begin(115200);
+    delay(5000);
+    Serial.printf("Starting up. time:%6ld \n",millis());
+    pinMode(relayPin, OUTPUT);
+    state = 0;
+    pumpticker.attach_ms_scheduled(period, relay_pin_state);
 }
 
 void loop(){
-  if (state){
-    digitalWrite(relayPin, HIGH);
+    if (state){
+        digitalWrite(relayPin, LOW);
+        Serial.printf("digitalWrite(relayPin, LOW) time:%6ld \n",millis());
+        unsigned long start_time = millis();
+        while( (millis()-start_time) < execution){
+            //let esp8266 run background functions.
+            yield();
+        }
+        digitalWrite(relayPin, HIGH);
+        Serial.printf("digitalWrite(relayPin, HIGH) time:%6ld \n",millis());
+        state = 0;
   }
 }
